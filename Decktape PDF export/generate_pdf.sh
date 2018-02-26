dbaf1901586bd8c58b2d5b78d09dc39a6c4cc2be
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2018-02-26 12:06:33 +0100

    Added Descktape PDF exporter Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

---
+++ Decktape PDF export/generate_pdf.sh
@@ -0,0 +1,6 @@
+# Run the reveal.js slides in a browser (without the Netbeans connector) and specify the URL variable
+#######################################################################################################
+
+URL=http://localhost:8383/slides/index.html#/
+
+decktape-1.0.0/phantomjs decktape-1.0.0/decktape.js automatic $URL slides.pdf
